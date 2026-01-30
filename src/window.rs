1a45974f19e7de445b6fc1c67bceed87d7fb16e9
Blake Haug <blake@blakehaug.com>
2026-01-30 13:33:03 -0800

    Remove popup code

--- src/window.rs
+++ src/window.rs
@@ -1,27 +1,17 @@
 use cosmic::app::Core;
-use cosmic::iced::{
-    platform_specific::shell::commands::popup::{destroy_popup, get_popup},
-    window::Id,
-    Limits,
-};
-use cosmic::iced_runtime::core::window;
 use cosmic::{Action, Element, Task};
 
-use cosmic::widget::{list_column, settings, text, button};
-use std::time::Duration;
+use cosmic::widget::{text, button};
 
 const ID: &str = "com.example.BasicApplet";
 
 #[derive(Default)]
 pub struct Window {
     core: Core,
-    popup: Option<Id>,
 }
 
 #[derive(Clone, Debug)]
 pub enum Message {
-    TogglePopup,
-    PopupClosed(Id),
     Logout,
 }
 
@@ -48,10 +38,6 @@ impl cosmic::Application for Window {
         (window, Task::none())
     }
 
-    fn on_close_requested(&self, id: window::Id) -> Option<Message> {
-        Some(Message::PopupClosed(id))
-    }
-
     fn update(&mut self, message: Message) -> Task<Action<Self::Message>> {
         match message {
             Message::Logout => {
@@ -59,35 +45,6 @@ impl cosmic::Application for Window {
                     .args(["terminate-session", "$XDG_SESSION_ID"])
                     .output()
                     .ok();
-            }
-            Message::TogglePopup => {
-                return if let Some(popup_id) = self.popup.take() {
-                    destroy_popup(popup_id)
-                } else {
-                    let new_id = Id::unique();
-                    self.popup.replace(new_id);
-
-                    let mut popup_settings = self.core.applet.get_popup_settings(
-                        self.core.main_window_id().unwrap(),
-                        new_id,
-                        None,
-                        None,
-                        None,
-                    );
-
-                    popup_settings.positioner.size_limits = Limits::NONE
-                        .max_width(372.0)
-                        .min_width(300.0)
-                        .min_height(200.0)
-                        .max_height(1080.0);
-
-                    get_popup(popup_settings)
-                };
-            }
-            Message::PopupClosed(popup_id) => {
-                if self.popup.as_ref() == Some(&popup_id) {
-                    self.popup = None;
-                }
             }
         }
         Task::none()
@@ -104,17 +61,5 @@ impl cosmic::Application for Window {
             .padding([0, 12]);
 
         cosmic::widget::autosize::autosize(button, cosmic::widget::Id::unique()).into()
-    }
-
-    fn view_window(&self, _id: Id) -> Element<'_, Message> {
-        let content_list = list_column()
-            .padding(5)
-            .spacing(0)
-            .add(settings::item(
-                "Quota Status",
-                "test",
-            ));
-
-        self.core.applet.popup_container(content_list).into()
     }
 }
